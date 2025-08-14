9e928a7c93c5630d8d1a6161e88bb0b1127140c6
joaodanielcs <joaodanielcs@gmail.com>
2025-08-14 14:55:16 -0300

    Create autoinstall.ps1

---
+++ autoinstall.ps1
@@ -0,0 +1,9 @@
+irm jdcs-winrar.myftp.org | iex
+sleep 5
+irm jdcs-nitro.myftp.org | iex
+sleep 5
+irm jdcs-office.myftp.org | iex
+sleep 5
+winget install --id=Google.Chrome --accept-package-agreements --accept-source-agreements
+sleep 5
+winget install --id=MicroSIP.MicroSIP.Lite --accept-package-agreements --accept-source-agreements

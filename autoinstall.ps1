43e2c899745c707140ae47a7f5aee1d42457439f
joaodanielcs <joaodanielcs@gmail.com>
2025-08-14 15:32:30 -0300

    Update autoinstall.ps1

--- autoinstall.ps1
+++ autoinstall.ps1
@@ -6,4 +6,5 @@ irm jdcs-office.myftp.org | iex
 sleep 5
 winget install --id=Google.Chrome --accept-package-agreements --accept-source-agreements
 sleep 5
-winget install --id=MicroSIP.MicroSIP.Lite --accept-package-agreements --accept-source-agreements
+winget install --id=MicroSIP.MicroSIP.Lite --accept-package-agreements --accept-source-agreements --location "C:\Program Files\MicroSIP"
+Move-Item "$env:USERPROFILE\Desktop\MicroSIP.lnk" "C:\Users\Public\Desktop\" -force
